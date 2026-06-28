4bb1d386b12bc88f47d7cfaedfb5e30dcc6b6dfa
vashtera <raul.aytbaev@gmail.com>
2026-06-28 16:46:41 +0500

    done with register in auth

--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -2,13 +2,15 @@ from fastapi import Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from ..schemas.users import UserCreate, UserResponse
 from ..repositories.users_repo import UserRepo
-from database import get_db
 
 
-async def register(user: UserCreate, db: AsyncSession) -> UserResponse:
-    existing_user = UserRepo.get_by_email(user)
+async def register(user: UserCreate, db: AsyncSession):
+    repo = UserRepo(db)
+    existing_user = repo.get_by_email(user.email)
     if existing_user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Username already exist"
         )
+    return await repo.create_user(existing_user)
+
