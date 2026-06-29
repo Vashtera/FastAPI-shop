9d0940cf5ae0bd31257b4401820e750902191dec
vashtera <raul.aytbaev@gmail.com>
2026-06-29 17:17:50 +0500

    change main

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,4 +3,8 @@ from routers.users import router as users_router
 
 app = FastAPI()
 
-app.include_router(users_router)
+app.include_router(users_router, prefix="/users", tags=["users"])
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to FastAPI-Shop"}
