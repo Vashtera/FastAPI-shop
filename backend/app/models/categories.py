224ad96cfeab8315ba44d8a85c60f4e52191fad2
vashtera <raul.aytbaev@gmail.com>
2026-07-05 17:01:36 +0500

    small fixes in models

--- backend/app/models/categories.py
+++ backend/app/models/categories.py
@@ -1,7 +1,6 @@
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..database import Base
-from ..models.products import Product
 
 class Category(Base):
     __tablename__ = "category"
@@ -10,7 +9,7 @@ class Category(Base):
     name: Mapped[str] = mapped_column(String(128), unique=True, index=True)
     slug: Mapped[str] = mapped_column(String(128), unique=True, index=True)
 
-    products: Mapped["Product"] = relationship(back_populates="category")
+    products: Mapped[list["Product"]] = relationship(back_populates="category")
 
     def __repr__(self) -> str:
         return f"<Category(id={self.id}, name='{self.name}')>"
--- backend/app/models/products.py
+++ backend/app/models/products.py
@@ -2,20 +2,19 @@ from datetime import datetime
 from sqlalchemy import String, ForeignKey, Text, DECIMAL, Date
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..database import Base
-from ..models.categories import Category
 
 class Product(Base):
     __tablename__ = "product"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
+    category_id: Mapped[int] = mapped_column(ForeignKey("category.id"))
     name: Mapped[str] = mapped_column(String(128), nullable=False, index=True)
     description: Mapped[str] = mapped_column(Text)
     price: Mapped[float] = mapped_column(DECIMAL, nullable=False)
     created_at: Mapped[datetime] = mapped_column(Date, default=datetime.utcnow)
     image_url: Mapped[str] = mapped_column(String)
 
-    category: Mapped["Category"] = relationship(back_populates="product")
+    category: Mapped["Category"] = relationship(back_populates="products")
 
     def __repr__(self) -> str:
         return f"<Product(id={self.id}, name='{self.name}', price={self.price})>"
