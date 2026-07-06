212ccde7dfae0c6ee25e9a17d0320637a85ec39f
vashtera <raul.aytbaev@gmail.com>
2026-07-06 14:18:07 +0500

    small fixes in products router

--- backend/app/routers/products.py
+++ backend/app/routers/products.py
@@ -16,14 +16,14 @@ def get_prod_service(session = Depends(get_session)):
 
 @router.get("", response_model=ProductListResponse, status_code=status.HTTP_200_OK)
 async def get_all_products(service: prod_service = Depends(get_prod_service)):
-    return service.get_all()
+    return await service.get_all()
 
 
 @router.get("/{product_id}", response_model=ProductResponse, status_code=status.HTTP_200_OK)
 async def get_product(product_id, service: prod_service = Depends(get_prod_service)):
-    return service.get_by_product_id(product_id)
+    return await service.get_by_product_id(product_id)
 
 
 @router.get("/category/{category_id}", response_model=ProductResponse, status_code=status.HTTP_200_OK)
 async def get_product_by_category(category_id: int, service: prod_service = Depends(get_prod_service)):
-    return service.get_by_category_id(category_id)
+    return await service.get_by_category_id(category_id)
