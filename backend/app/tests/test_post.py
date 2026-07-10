c70e2cac2a9274009b12fb028f33848e08df1975
vashtera <raul.aytbaev@gmail.com>
2026-07-10 11:54:48 +0500

    add login test

--- backend/app/tests/test_post.py
+++ backend/app/tests/test_post.py
@@ -1,6 +1,6 @@
-import pytest
 from httpx import AsyncClient
 
+# Правильные данные
 async def test_register(client: AsyncClient):
     response = await client.post("/users/registration/", json={
         "first_name": "Raul",
@@ -10,3 +10,21 @@ async def test_register(client: AsyncClient):
     })
     assert response.status_code == 200
     assert response.json()["first_name"] == "Raul"
+
+
+# Правильные данные
+async def test_login(client: AsyncClient):
+    # Сначала нужно зарегистрировать пользователя
+    await client.post("/users/registration/", json={
+        "first_name": "Raul",
+        "last_name": "Aitbayev",
+        "email": "raul2@test.com",
+        "password": "12345678"
+    })
+    
+    response = await client.post("/users/login/", data={ # OAuth2 использует data, не json
+        "username": "raul2@test.com",  # OAuth2 использует username, не email
+        "password": "12345678"
+    })
+    assert response.status_code == 200
+    assert "access_token" in response.json()
