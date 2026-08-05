94dcf652cada703619dd2e3d26d7f9eb7ecbaa3c
vashtera <raul.aytbaev@gmail.com>
2026-08-05 15:43:22 +0500

    change endpoints

--- backend/app/routers/cart.py
+++ backend/app/routers/cart.py
@@ -13,20 +13,20 @@ router = APIRouter(
 def get_cart_service(session = Depends(get_session)):
     return CartService(session, settings.redis_url, settings.cache_ttl_seconds)
 
-
+# TODO Pylance может ругаться на валидацию данных в параметрах роутера
 @router.get("", response_model=CartResponse, status_code=status.HTTP_200_OK)
 async def get_cart(
-    user_id = Depends(get_current_user), 
+    user_id: int = Depends(get_current_user), 
     service: CartService = Depends(get_cart_service)
     ):
     return await service.get_cart_details(user_id.id)
 
 
-@router.post("/add", status_code=status.HTTP_200_OK)
+@router.post("/add/", status_code=status.HTTP_200_OK)
 async def add_to_cart(
     product_id: int, 
     quantity: int,
-    user_id = Depends(get_current_user), 
+    user_id: int = Depends(get_current_user), 
     service: CartService = Depends(get_cart_service)
     ):
     item = CartCreate(product_id=product_id, quantity=quantity)
@@ -34,11 +34,11 @@ async def add_to_cart(
     return {'cart': updated_cart}
 
 
-@router.put("/update", status_code=status.HTTP_200_OK)
+@router.put("/update/", status_code=status.HTTP_200_OK)
 async def update_cart(
     product_id: int, 
     quantity: int,
-    user_id = Depends(get_current_user), 
+    user_id: int = Depends(get_current_user), 
     service: CartService = Depends(get_cart_service)
     ):
     item = CartItemUpdate(product_id=product_id, quantity=quantity)
@@ -46,10 +46,10 @@ async def update_cart(
     return {'cart': updated_cart}
 
 
-@router.delete("/remove/{product_id}", status_code=status.HTTP_200_OK)
+@router.delete("/remove/{product_id}/", status_code=status.HTTP_200_OK)
 async def remove_from_cart(
     product_id: int,
-    user_id = Depends(get_current_user), 
+    user_id: int = Depends(get_current_user), 
     service: CartService = Depends(get_cart_service)
     ):
     updated_cart = await service.delete_from_cart(user_id.id, product_id)
--- backend/app/routers/categories.py
+++ backend/app/routers/categories.py
@@ -20,12 +20,12 @@ async def get_categories(service: ctg_service = Depends(get_ctg_service)):
     return await service.get_all_categories()
 
 
-@router.get("/{category_id}", response_model=CategoryResponse, status_code=status.HTTP_200_OK)
+@router.get("/{category_id}/", response_model=CategoryResponse, status_code=status.HTTP_200_OK)
 async def get_category(category_id: int, service: ctg_service = Depends(get_ctg_service)):
     return await service.get_by_category_id(category_id)
 
 
-@router.post("/add", response_model=CategoryResponse, status_code=status.HTTP_201_CREATED)
+@router.post("/add/", response_model=CategoryResponse, status_code=status.HTTP_201_CREATED)
 async def create_category(
     category_data: CategoryCreate, 
     service: ctg_service = Depends(get_ctg_service),
